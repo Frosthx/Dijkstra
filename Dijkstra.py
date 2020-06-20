2f1131dae5b9c28bec42b9e1e549ccd3245b7b53
Frosthx <1006739119@qq.com>
2020-06-20 15:36:47 +0800

    noPath

--- Dijkstra.py
+++ Dijkstra.py
@@ -1,18 +1,25 @@
-def dijkstra(graph, src, end):
+def dijkstra(graph, src, dst):
     if graph == None:
         return None
-    
-
-
-
-
-
-
-
-
-
-
-
+    nodes = [i for i in range(len(graph))]
+    dist = [float('inf')]*len(graph)
+    dist[src] = 0
+    dist_init = [i for i in graph[src]]
+    path = []
+    #path.append(src)
+    visited = []
+    visited.append(src)
+    while nodes:
+        nextVertexDist = min([vDist for vertex,vDist in enumerate(dist_init) if vertex in nodes])
+        nextVertex = dist_init.index(nextVertexDist)
+        path.append(nextVertex)
+        nodes.remove(nextVertex)
+        for vertex,vDist in enumerate(graph[nextVertex]):
+            if 0<vDist<inf:
+                if dist[vertex] > dist[nextVertex]+vDist:
+                    dist[vertex] = dist[nextVertex]+vDist
+                    dist_init[vertex] = dist[vertex]
+    return path,dist
 
 if __name__ == '__main__':
     # graph 图的邻接矩阵
@@ -27,3 +34,6 @@ if __name__ == '__main__':
         [inf, inf, 2.0, 9.0, inf, 4.0, 0.0, 2.0],
         [inf, inf, inf, inf, 9.0, 6.0, 2.0, 0.0]
     ]
+    path,distance = dijkstra(graph,0,7)
+    print(path)
+    print(distance)
